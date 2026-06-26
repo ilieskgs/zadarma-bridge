af3a8f2a5098a5bd5c750c0db42f822694631fd9
Ilies <ilieskgs@gmail.com>
2026-06-26 13:52:38 +0200

    fix HMAC SHA1 + register webhook SMS

--- index.js
+++ index.js
@@ -13,15 +13,42 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const conversations = {};
 
-async function sendSMS(to, message) {
-  const params = { number: to, message: message };
+async function zadarmaRequest(method, params) {
   const keys = Object.keys(params).sort();
-  const sortedStr = keys.map(function(k) { return k + '=' + params[k]; }).join('&');
-  const sign = crypto.createHmac('md5', ZADARMA_SECRET).update('/v1/sms/send/' + sortedStr).digest('hex');
-  const encoded = Buffer.from(ZADARMA_KEY + ':' + sign).toString('base64');
-  await axios.post('https://api.zadarma.com/v1/sms/send/', params, {
-    headers: { Authorization: 'Basic ' + encoded }
+  const paramsStr = keys.map(function(k) { return k + '=' + encodeURIComponent(params[k]).replace(/%20/g, '+'); }).join('&');
+  const sign = crypto.createHmac('sha1', ZADARMA_SECRET).update(method + paramsStr + crypto.createHash('md5').update(paramsStr).digest('hex')).digest('base64');
+  const header = ZADARMA_KEY + ':' + sign;
+  return axios.post('https://api.zadarma.com' + method, params, {
+    headers: { Authorization: header, 'Content-Type': 'application/x-www-form-urlencoded' }
   });
+const express = require('express');
+const axios = require('axios');
+const crypto = require('crypto');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const MAKE_URL = 'https://hook.eu2.make.com/srvppkgm0uezqykuwp6tbxrthw3t39u4';
+const ZADARMA_KEY = process.env.ZADARMA_KEY;
+const ZADARMA_SECRET = process.env.ZADARMA_SECRET;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const conversations = {};
+
+async function zadarmaRequest(method, params) {
+  const keys = Object.keys(params).sort();
+  const paramsStr = keys.map(function(k) { return k + '=' + encodeURIComponent(params[k]).replace(/%20/g, '+'); }).join('&');
+  const md5params = crypto.createHash('md5').update(paramsStr).digest('hex');
+  const sign = crypto.createHmac('sha1', ZADARMA_SECRET).update(method + paramsStr + md5params).digest('base64');
+  const header = ZADARMA_KEY + ':' + sign;
+  return axios.post('https://api.zadarma.com' + method, paramsStr, {
+    headers: { Authorization: header, 'Content-Type': 'application/x-www-form-urlencoded' }
+  });
+}
+
+async function sendSMS(to, message) {
+  await zadarmaRequest('/v1/sms/send/', { number: to, message: message });
 }
 
 async function askGPT(phone, userMessage) {
@@ -76,4 +103,9 @@ app.all('/', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function() { console.log('Serveur demarre sur port ' + PORT); });
+app.listen(PORT, function() {
+  console.log('Serveur demarre sur port ' + PORT);
+  zadarmaRequest('/v1/pbx/webhooks/url/', { url: 'https://zadarma-bridge-production.up.railway.app' })
+    .then(function(r) { console.log('Webhook SMS enregistre:', r.data); })
+    .catch(function(e) { console.log('Erreur webhook:', e.message); });
+});;
