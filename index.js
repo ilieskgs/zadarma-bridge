ce7edfa808ed39907ba84b6c32b3bdecb2c3a443
Ilies <ilieskgs@gmail.com>
2026-06-26 12:54:04 +0200

    fix erreur listen

--- index.js
+++ index.js
@@ -78,4 +78,4 @@ app.all('/', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Serveur demarre sur port ${PORT}`));s
+app.listen(PORT, () => console.log('Serveur demarre sur port ' + PORT));s
